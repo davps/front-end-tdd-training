e6cf4cdcd8659c7f7c8d109306dccfd953cebc0e
davps <davidperez3010@gmail.com>
2018-04-21 15:02:36 -0400

    Update

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -3,7 +3,9 @@ import { List } from 'immutable';
 export default function reducer(state, action) {
   if (action.type === 'ADD_ROW') {
     var row = { id: 1, order: 1 };
-    List(state.rows).splice(action.afterID + 1, 0, row);
+    const rows = List(state.rows).splice(action.afterID + 1, 0, row);
+    const newState = {...state, rows};
+    return newState;
   }
 
   return state;
