4e07e66569c859433ce2038e579851ec3b90ff6c
davps <davidperez3010@gmail.com>
2018-04-21 15:08:59 -0400

    Update

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -4,7 +4,9 @@ export default function reducer(state, action) {
   if (action.type === 'ADD_ROW') {
     const id = Math.max(0, ...state.map(o => o.id)) + 1;
     var row = { id, order: 1 };
-    const rows = List(state.rows).splice(action.afterID + 1, 0, row);
+    const rows = List(state.rows)
+        .splice(action.afterID + 1, 0, row)
+        .map((o, order)=>({...o, order}));
     const newState = { ...state, rows };
     return newState;
   }
