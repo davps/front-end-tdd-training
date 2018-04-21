2e8286736d504491e9a64f0f5742b8b10327f882
davps <davidperez3010@gmail.com>
2018-04-21 15:22:25 -0400

    Update

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -2,7 +2,7 @@ import { List } from 'immutable';
 
 export default function reducer(state, action) {
   if (action.type === 'ADD_ROW') {
-    const id = Math.max(0, ...state.rows.map(o => o.id)) + 1;
+    const id = Math.max(-1, ...state.rows.map(o => o.id)) + 1;
     var row = { id, order: 1 };
     const rows = List(state.rows)
         .splice(action.afterID + 1, 0, row)
