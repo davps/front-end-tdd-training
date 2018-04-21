d84d8f827a47b5e9f0bc7d530abf60929b3b9d12
davps <davidperez3010@gmail.com>
2018-04-21 13:54:23 -0400

    Update

--- src/actions/ADD_ROW.test.js
+++ src/actions/ADD_ROW.test.js
@@ -2,16 +2,14 @@
 // When I request to add a new row.
 // Then render one row.
 
-import initState from '../initState'
+import initState from '../initState';
 
-describe("When I request to add a new row", function() {
-    beforeEach(function () {
-        
-    })
-    it("Then render one row", function () {
-        var afterID = 1;
-        var action = addRow(afterID);
-        const state = reducer(initState, action);
-        expect(true).toBe(true);
-    });
+describe('When I request to add a new row', () => {
+  beforeEach(() => {});
+  it('Then render one row', () => {
+    var afterID = 1;
+    var action = {afterID: 1};
+    const state = reducer(initState, action);
+    expect(true).toBe(true);
+  });
 });
