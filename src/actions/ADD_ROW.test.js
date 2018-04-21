ac5ef8cfc8eae4af1707514cb0736bb484b6f7d0
davps <davidperez3010@gmail.com>
2018-04-21 14:03:19 -0400

    Update

--- src/actions/ADD_ROW.test.js
+++ src/actions/ADD_ROW.test.js
@@ -9,14 +9,13 @@ describe('When I request to add a new row', () => {
   beforeEach(() => {});
   it('Then we should have one row', () => {
     expect(initState.row).toEqual([]);
-    var afterID = 1;
-    var action = {type: 'ADD_ROW', afterID: 1};
+
+    var action = { type: 'ADD_ROW', afterID: 1 };
     const state = reducer(initState, action);
+
     expect(state).toBeDefined();
     expect(state.rows).toBeDefined();
     expect(state.rows.length).toBe(1);
-    expect(state.rows).toEqual([
-        {id: 0, order: 0}
-    ])
+    expect(state.rows).toEqual([{ id: 0, order: 0 }]);
   });
 });
