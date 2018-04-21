963f466a7e626cf6c33391028cc128300aaf320f
davps <davidperez3010@gmail.com>
2018-04-21 13:22:30 -0400

    Init state

---
+++ src/initState.js
@@ -0,0 +1,17 @@
+export const initState = {
+  hover: -1,
+  rows: [
+    {
+      id: 5,
+      order: 0
+    },
+    {
+      id: 1,
+      order: 2
+    },
+    {
+      id: 3,
+      order: 1
+    }
+  ]
+};
