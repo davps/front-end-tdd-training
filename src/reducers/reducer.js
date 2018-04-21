382a2702438c67d8d2a717fff023b1828556d77a
davps <davidperez3010@gmail.com>
2018-04-21 14:58:24 -0400

    Update

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,6 +1,8 @@
 import {List} from 'immutable';
+
 export default function reducer(state, action) {
   if (action.type === 'ADD_ROW') {
+    List(state.rows).splice(index: number, removeNum: number, ...values: Array<T>): this
 
   }
 
