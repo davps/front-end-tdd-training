e5000063da747ef9bea94b8fdcd3277bf7f382f2
davps <davidperez3010@gmail.com>
2018-04-21 15:33:57 -0400

    Update

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,25 +1,30 @@
 import { List } from 'immutable';
 
-export default function reducer(state, action) {
+function reducerRows(state, action) {
   if (action.type === 'ADD_ROW') {
-    if(typeof action.afterID !== 'number'){
-        throw new Error('afterID must be numeric');
+    if (typeof action.afterID !== 'number') {
+      throw new Error('afterID must be numeric');
     }
 
-    if(!state.find(o=>o.id === action.afterID)){
-        throw
+    if (!state.find(o => o.id === action.afterID) && action.afterID === -1) {
+      throw new Error('afterID not valid');
     }
 
+    if (state.rows !== null) {
+    }
 
     const id = Math.max(-1, ...state.rows.map(o => o.id)) + 1;
     var row = { id, order: 1 };
     const rows = List(state.rows)
-        .splice(action.afterID + 1, 0, row)
-        .map((o, order)=>({...o, order}))
-        .toArray();
+      .splice(action.afterID + 1, 0, row)
+      .map((o, order) => ({ ...o, order }))
+      .toArray();
     const newState = { ...state, rows };
     return newState;
   }
-
-  return state;
+    
+}
+export default function reducer(state, action) {
+  var rows = reducerRows(state.rows, action);
+  return {...state, rows};
 }
