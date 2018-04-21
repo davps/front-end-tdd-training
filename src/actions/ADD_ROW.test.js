46be7d97cc72faa2d7d97bfbd3f2ea32ce7701de
davps <davidperez3010@gmail.com>
2018-04-21 13:57:31 -0400

    Update

--- src/actions/ADD_ROW.test.js
+++ src/actions/ADD_ROW.test.js
@@ -8,7 +8,7 @@ describe('When I request to add a new row', () => {
   beforeEach(() => {});
   it('Then render one row', () => {
     var afterID = 1;
-    var action = {afterID: 1};
+    var action = {type: 'ADD_ROW', afterID: 1};
     const state = reducer(initState, action);
     expect(true).toBe(true);
   });
---
+++ src/reducers/reducer.js
@@ -0,0 +1,7 @@
+export default function reducer(state, action){
+    if(action.type === 'ADD_ROW'){
+
+    }
+
+    return state;
+}
